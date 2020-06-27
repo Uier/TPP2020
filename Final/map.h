a8f18acf52cbfd9e7939f73f1b2b7855ce2e5d45
Uier <ap9940506@gmail.com>
2020-06-27 22:56:45 +0800

    Final

---
+++ Final/map.h
@@ -0,0 +1,44 @@
+class MapUnit {
+
+public:
+
+private:
+
+};
+
+
+class UpgradableUnit : public MapUnit {
+	
+public:
+
+private:
+
+};
+
+
+class CollectableUnit : public MapUnit {
+	
+public:
+
+private:
+
+};
+
+
+class RandomCostUnit : public MapUnit {
+	
+public:
+
+private:
+
+};
+
+
+class JailUnit : public MapUnit {
+	
+public:
+
+private:
+
+};
+
